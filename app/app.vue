24917a0f608b8120f9ee0fadfdf8067d8b3b1949
MaksimIvanyuhin <chelovechek.maks@mail.ru>
2026-03-29 13:49:21 +0300

    Добавил условие для подчеркивания текста в текстовом компоненте

--- app/app.vue
+++ app/app.vue
@@ -1,5 +1,7 @@
 <template>
   <div>
     <LoremIpsum />
+    <br>
+    <LoremIpsum :underline="true" />
   </div>
 </template>
--- app/components/LoremIpsum.vue
+++ app/components/LoremIpsum.vue
@@ -1,6 +1,11 @@
 <template>
   <MainContainer>
-    <p class="lorem-ipsum">
+    <p
+      :class="{
+        'lorem-ipsum': true,
+        'lorem-ipsum_underline': props.underline,
+      }"
+    >
       Lorem ipsum dolor sit amet consectetur, adipisicing elit. Magnam, at.
     </p>
   </MainContainer>
@@ -17,4 +22,13 @@ p {
   font-size: clamp(1rem, 3vw, 2rem);
   font-weight: 700;
 }
+.lorem-ipsum_underline {
+  text-decoration: underline;
+}
 </style>
+
+<script setup>
+const props = defineProps({
+  underline: Boolean,
+})
+</script>
